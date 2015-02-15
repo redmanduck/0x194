7da3fac72afd34ac4c3623edb1dca4fc81a442fc
Pat Sabpisal <ssabpisa@me.com>
2015-02-15 02:10:04 -0500

    ..

--- ece404_hw04_ssabpisa.py
+++ ece404_hw04_ssabpisa.py
@@ -27,9 +27,6 @@ class KeySchedule:
         self.statearray = [[None for x in range(self.state_dim)] for x in range(self.state_dim)]
         self.keybv = BitVector(textstring = keystr)
         print "key - " , _hex(self.keybv)
-        self.expand_and_fill(0)
-
-    def expand_and_fill(self, offset):
         # generate NxN state matrix
         for i in range(self.state_dim):
             for j in range(self.state_dim):
@@ -40,22 +37,44 @@ class KeySchedule:
                 print "S"+str(i)+","+str(j), " | ", self.statearray[i][j]
 
 
-        #the first four w0...w3
+        #the first four w0...dim-1
         for n in range(self.state_dim):
             wn = BitVector(size=0)
-            for i in range(self.state_dim):
-                wn += self.statearray[i][n]
+            for r in range(self.state_dim):
+                wn += self.statearray[r][n]
             self.xkey.append(wn)
+
+
+
+        # expand {w0...w3} to {w0...w43}
+        self.expand(0)
 
         print "----------- W0..W43 ----------------"
         for i, wrd in enumerate(self.xkey):
-            print 'w'+str(i), wrd
+            print 'w'+str(i),
+            print _hex(wrd)
+
+    def g(self, x):
+        return x
+    """
+        generate the next set of N words
+    """
+    def expand(self, offset):
+        N = self.state_dim
+
+        # self.xkey[0][i] gives i-th byte of w0
+        w0 = self.g(self.xkey[offset*N + (N - 1)]) ^ self.xkey[offset*N]
+        self.xkey.append(w0)
+
+        for i in range(1,N):
+            wi = self.g(self.xkey[i + offset*N]) ^ self.xkey[i-1 + offset*N]
+            self.xkey.append(wi)
 
     def generate_lookup_table(self):
         pass
 
 class AES:
-    def __init__(self, key="lukeimyourfather", keylength=128):
+    def __init__(self, key, keylength=128):
         pass
     def encrypt(self, text, keyschedule):
         pass
