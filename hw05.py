cc739616d2f71752581e07f7be39f9b030bd18cb
ssabpisa <ssabpisa@whatducklin.localdomain>
2015-02-23 16:16:34 -0500

    Started

--- hw05.py
+++ hw05.py
@@ -10,10 +10,10 @@ class RC4:
 		self.S = [i for i in range(256)] # State vector
 		self.T = [i for i in range(256)] # T vector
 		keylen = 16 # 16 bytes
-		
+
 		for i in range(256):
-			self.T[i] = self.K[i % keylen] 
-		
+			self.T[i] = self.K[i % keylen]
+
 		#Produce initial permutation of S  (KSA algorithm)
 		j = 0
 		for i in range(256):
@@ -22,23 +22,44 @@ class RC4:
 			temp = self.S[i]
 			self.S[i] = self.S[j]
 			self.S[j] = temp
-		
-		del self.T 
-	  	
-		self.PBS = []	 #pseudorandom byte stream
+
+		del self.T
+
+	def encrypt(self,Image):
 		i =  0
 		j = 0
-		while True:
-			i = (i+1) % 256
-			j = (j+self.S[i]) % 256
-			temp  =	self.S[i]
-			self.S[i] = self.S[j]
-			self.S[j] = temp
-			k = (self.S[i] + self.S[j]) % 256
-			print self.S[k]
+		for r in Image:
+			for c in Image:
+				i = (i+1) % 256
+				j = (j+self.S[i]) % 256
+				temp  =	self.S[i]
+				self.S[i] = self.S[j]
+				self.S[j] = temp
+				k = (self.S[i] + self.S[j]) % 256
+				random_byte = self.S[k]
 
-	def encrypt(Image):
-		pass
+				# print "RBYTE" , random_byte
+				# print "IBYTE" , Image[r][c]
+
+	@staticmethod
+	def loadPPM(filename):
+		headers = []
+		image = []
+		f  = open(filename , "r")
+		data = f.readlines()
+		for i in range(len(data)):
+			curline = data[i]
+			if i < 5:
+				#header
+				headers.append(data[i])
+				continue
+			row = []
+			for j in range(len(curline)):
+				row.append(ord(curline[j]))
+			image.append(row)
+		return headers, image
 
 if __name__ == "__main__":
-	rc4 = RC4("abcdefghaaaabbbb")	
+	originalHead, originalImage = RC4.loadPPM("Tiger2.ppm")
+	rc4cipher = RC4("abcdefghaaaabbbb")
+	rc4cipher.encrypt(originalImage)
