8f1f1e16922db9e17840800da324b46b19e0d269
Pat Sabpisal <ssabpisa@me.com>
2015-03-11 15:01:07 -0400

    will look agian

--- rabinmiller.py
+++ rabinmiller.py
@@ -7,14 +7,17 @@ import sys
 def find_kq(p):
     # find some positive k and odd q that makes
     # p - 1 = 2^k * q
+    # (p-1)/q = 2^k
+    # dlog_2_( [p-1]/q  ) = k
+    # ask how many times does 2 have to repeat until it get to (p-1)/q
     k = 0
     q = p-1
-    while not q*1:
-        q >>= 1
+    while not q&1:
+        q >>= 1    # divide by 2 until not integer
         k += 1
     return k,q
 
-n  = 97
+n  = 389
 prime = False
 probes = [2,3,5,7,11,13,17]
 #compute a^m == +/- 1 mod n
